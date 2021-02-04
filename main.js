30293370d330520d94f87c3f97710fe4fcffa9e2
RomanticCoder <67548512+RomanticCoder@users.noreply.github.com>
2021-02-03 21:38:21 -0500

    html_head,icon

--- main.js
+++
@@ -1,7 +0,0 @@
-const bookmark = document.querySelector(".bookmark");
-const bookmark__items = document.querySelectorAll(".navbar__bookmark__item");
-bookmark.addEventListener("click", () => {
-  bookmark__items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-});
